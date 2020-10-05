c5e4f8ab645bccaa1d549eaa25726183396579e8
GmrMkrSkl24 <47901557+GmrMkrSkl24@users.noreply.github.com>
2020-10-04 23:53:26 -0400

    Animation commit

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -6,9 +6,9 @@ public class SoundScript : MonoBehaviour
 {
     public AudioClip musicClipOne;
 
-public AudioClip musicClipTwo;
+    public AudioClip musicClipTwo;
 
-public AudioSource musicSource;
+    public AudioSource musicSource;
 
     // Start is called before the first frame update
     void Start()
